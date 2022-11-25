ae528d42d78106166c567704bdcb4942f119f5eb
Gui Sarmento <gui@memberful.com>
2022-11-25 19:53:24 +0000

    Fix links

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -3,9 +3,9 @@ require "middleman-core"
 module CustomHelpers
   def podcast_mp3_url(podcast)
     if podcast.data.podcast_sample_mp3
-      "/podcast_mp3/samples/#{podcast.data.podcast_sample_mp3}"
+      "podcast_mp3/samples/#{podcast.data.podcast_sample_mp3}"
     elsif podcast.data.podcast_complete_mp3
-      "/podcast_mp3/complete/#{podcast.data.podcast_complete_mp3}"
+      "podcast_mp3/complete/#{podcast.data.podcast_complete_mp3}"
     end
   end
 
